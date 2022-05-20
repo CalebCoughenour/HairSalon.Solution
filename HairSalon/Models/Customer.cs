9b56edcd3b0d7632b190403c20ad705f528545e1
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-20 15:41:38 -0700

    revert property changes for both models

--- HairSalon/Models/Customer.cs
+++ HairSalon/Models/Customer.cs
@@ -1,18 +1,15 @@
+using System.Collections.Generic;
+
 namespace HairSalon.Models
 {
   public class Customer
   {
 
-    public Customer()
-    {
-      this.Stylists = new HashSet<Stylist>();
-    }
     public int CustomerId { get; set; }
     public string Name { get; set; }
 
     public string ApptDate { get; set; }
-    public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
-    public virtual ICollection<Stylist> Stylists { get; set; }
+  
   }
 }
