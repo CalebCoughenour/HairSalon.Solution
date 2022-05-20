cfea2085b07d56989382335422c3cbc89aeb952f
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-20 09:14:14 -0700

    add properties for stylist

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+  public class Stylist
+  {
+    public Stylist()
+    {
+      this.Customer = new HashSet<Customer>();
+    }
+
+    public string Name { get; set; }
+    public string Specialty { get; set; }
+    public int StylistId { get; set; }
+    public virtual ICollection<Customer> Customers { get; set; }
+  }
+}
