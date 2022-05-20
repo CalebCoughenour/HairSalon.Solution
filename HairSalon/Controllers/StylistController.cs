33bf87c4fbde9ae6d6ceed8dfd5cd77753e0a148
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-20 09:31:58 -0700

    add database connection to controller

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class StylistController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public StylistController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+  }
+}
